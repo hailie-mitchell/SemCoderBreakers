7af7950520d2a1ca1a7331806688a164e833f289
Ruyi Chen <rc3593@columbia.edu>
2024-11-02 00:42:04 -0400

    perturbed CRUXEval (func renamed) with log

--- cruxeval/command.sh
+++ cruxeval/command.sh
@@ -9,3 +9,8 @@ python run_robust.py create_partial natgen --datasets cruxeval
 
 # initial attempt to perturb CRUXEval
 python run_robust.py perturb natgen --aug_method 0 --datasets cruxeval
+
+# full perturbation
+python run_robust.py perturb func_name --datasets cruxeval
+python run_robust.py perturb natgen --datasets cruxeval
+python run_robust.py perturb format --datasets cruxeval
